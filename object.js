92386468a52066260e6ea8f3ae147eb16b366473
shajith26 <mohamedshajith321@gmail.com>
2022-08-30 14:50:32 +0530

    task

---
+++ object.js
@@ -0,0 +1,46 @@
+// Object 
+{
+	let person={
+		name: "rocky",
+		age: 30,
+		gender: "male",
+		address: "india"
+	}
+	console.log(person)
+	console.log(person.name)
+}
+{
+	let brand={
+		name: "otto",
+		materials:"shirt,t-shirt,phant",
+		address: "Tenkasi"
+	}
+	document.write(brand.name)
+}
+{
+	let bike={
+		model:"KTM rc 390",
+		color:"orange and white"
+	}
+	console.log(bike)
+	console.log(bike.model)
+}
+{
+	let mobile={
+		model:"samsung f62",
+		battery:"inbuild",
+		camera:"48 pixel",
+		display:"super amoled display"
+	}
+	console.log(mobile)
+	console.log(mobile.model)
+}
+{
+	let note={
+		brand:"classmate",
+		pages:"180 pages",
+		type:"unruled"
+	}
+	console.log(note)
+	console.log(note.brand)
+}
