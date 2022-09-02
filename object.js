bf9eef2c6d517aa5369ff37ff0477968aa70c291
shajith26 <mohamedshajith321@gmail.com>
2022-09-02 09:31:56 +0530

    demo

--- object.js
+++ object.js
@@ -10,12 +10,13 @@
 	console.log(person.name)
 }
 {
-	let brand={
+	let wear={
 		name: "otto",
 		materials:"shirt,t-shirt,phant",
 		address: "Tenkasi"
 	}
-	document.write(brand.name)
+	console.log(wear)
+	console.log(wear.name)
 }
 {
 	let bike={
@@ -43,4 +44,49 @@
 	}
 	console.log(note)
 	console.log(note.brand)
+}
+{
+	let school={
+		name:"bharath",
+		place:"ilanji",
+		sylabus:"state board and CBSE"
+	}
+	console.log(school)
+	console.log(school.name)
+}
+{
+	let pen={
+		brand:"skilly",
+		color:"blue",
+		type:"plastic"
+	}
+	console.log(pen)
+	console.log(pen.brand)
+}
+{
+	let bag={
+		brand:"sky bag",
+		water_resistance: true,
+		color:"blue"
+	}
+	console.log(bag)
+	console.log(bag.brand)
+}
+{
+	let laptop={
+		brand:"lenova",
+		model:"IdeaPad S145",
+		color:"grey"
+	}
+	console.log(laptop)
+	console.log(laptop.brand)
+}
+{
+	let headset={
+		brand:"boat",
+		color:"red",
+		type:"type c"
+	}
+	console.log(headset)
+	console.log(headset.brand)
 }
